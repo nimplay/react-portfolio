1df65b4a13bed6a36f8e6c434c1f9b8dfa3e6ee2
nimplay <nimrod7day@gmail.com>
2022-10-05 11:40:30 -0400

    add client testimonies

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -6,6 +6,7 @@ import 'swiper/css';
 import "swiper/css/pagination";
 import David from '../../img/david.jpeg'
 import Mirwil from '../../img/mirwil.jpeg'
+import John from '../../img/john.jpeg'
 import { themeContext } from '../../Context';
 import { useContext } from 'react';
 
@@ -15,12 +16,16 @@ const Testimonials = () => {
   const darkMode = theme.state.darkMode;
   const client = [
     {
-      img : David,
-      review : "My company has been working with Nimrod Acosta since 2019.  Thanks to that my webpage has evolved in a very positive way through the years. I recently worked with him to update my website. I have been mainly working with Nimrod . We are beyond pleased with Nimrod's impeccable service. He is extremely helpful in guiding us through the whole process. He is very knowledgeable. He pays close attention to details. He is also very patient in training us on how to use the new administration system. Overall, we are very pleased with Nimrod Acosta Web design.",
+      img : John,
+      review : "Nimrod is a good mentor, he has taken me through almost every phase of my software journey. He is a go-to whenever I have blockers in my projects and he always shows concern and offers good technical and moral support. A good teacher and a great listener, I highly recommend 💯.",
     },
     {
       img: Mirwil,
       review : "The reliable services of Mr. Nimrod Acosta are vital in my business as an illustrator.  Delegating all the technical (and sometimes tedious for me!) aspects of maintaining my website and online store is a relief, as it allows me to stay focused on my creative work, knowing that everything is in order.",
+    },
+    {
+      img : David,
+      review : "My company has been working with Nimrod Acosta since 2019.  Thanks to that my webpage has evolved in a very positive way through the years. I recently worked with him to update my website. I have been mainly working with Nimrod . We are beyond pleased with Nimrod's impeccable service. He is extremely helpful in guiding us through the whole process. He is very knowledgeable. He pays close attention to details. He is also very patient in training us on how to use the new administration system. Overall, we are very pleased with Nimrod Acosta Web design.",
     }
   ];
 
